6888dce94c809f87d2ef47476c97285b89444968
Moogah <keyla@moogah.com>
2018-07-06 17:04:03 -0300

    update

--- account-finantial-tools1/account_fiscal_year/__manifest__.py
+++ account-finantial-tools1/account_fiscal_year/__manifest__.py
@@ -3,20 +3,18 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Fiscal Year',
-    'version': '10.0.1.0.0',
-    'category': 'Accounting',
-    'author': 'Moogah',
-    		  'Camptocamp SA',
-              'Odoo Community Association OCA',
-    'website': 'http://www.moogah.com',
+    'name': "Account Fiscal Year",
+    'version': "10.0.1.0.0",
+    'category': "Accounting",
+    'author': "Moogah, Camptocamp SA, Odoo Community Association (OCA)",
+    'website': "http://www.moogah.com",
     'depends': [
-        'account',
-        'date_range'
+        "account",
+        "date_range"
     ],
     'data': [
-        'data/date_range_type.xml',
-        'views/date_range_type.xml'
+        "data/date_range_type.xml",
+        "views/date_range_type.xml"
     ],
     'installable': True,
     'application': True,
